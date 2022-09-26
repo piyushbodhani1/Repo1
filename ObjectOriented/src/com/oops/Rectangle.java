ce9dd9b18de86c3b6d3c5c5726cf67471cc1c3dc
piyushbodhani1 <piyush.bodhani@kpipartners.com>
2022-09-26 17:55:09 +0530

    My Sixth commit

---
+++ ObjectOriented/src/com/oops/Rectangle.java
@@ -0,0 +1,10 @@
+package com.oops;
+
+public class Rectangle extends Area {
+    public static void main(String a[])
+    {
+    	Rectangle r = new Rectangle();
+    	r.Reactangle1();
+    }
+}
+
---
+++ ObjectOriented/src/com/oops/Square.java
@@ -0,0 +1,9 @@
+package com.oops;
+
+public class Square extends Area{
+    public static void main(String a[])
+    {
+    	Square sq = new Square();
+    	sq.Square2();
+    }
+}
